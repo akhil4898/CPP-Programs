4a41b19ee911d5f89db557c3ee6aba592daa6e7f
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-10-15 11:52:37 +0530

    Update file

--- SDE/check.cpp
+++ SDE/check.cpp
@@ -6,43 +6,63 @@ using namespace std;
 
 int main()
 {
-    pair<int, string> p;
-    // p = make_pair(2, "Akhil");
-    cin >> p.first;
-    cin >> p.second;
-    // p = {2, "Akhil Upadhyay"};
-    pair <int, string> p1 = p;    // By value
-    p1.first = 10;
-    cout << p1.first << " " << p1.second << endl;
-    cout << p.first << " " << p.second << endl;
+
+// Pairs
+
+    // pair<int, string> p;
+    // // p = make_pair(2, "Akhil");
+    // cin >> p.first;
+    // cin >> p.second;
+    // // p = {2, "Akhil Upadhyay"};
+    // pair <int, string> p1 = p;    // By value
+    // p1.first = 10;
+    // cout << p1.first << " " << p1.second << endl;
+    // cout << p.first << " " << p.second << endl;
 
 
-    pair <int, string> &p2 = p;   // By reference
-    p2.second = "Nikhil";
-    cout << p2.first << " " << p2.second << endl;
-    cout << p.first << " " << p.second << endl << endl;
+    // pair <int, string> &p2 = p;   // By reference
+    // p2.second = "Nikhil";
+    // cout << p2.first << " " << p2.second << endl;
+    // cout << p.first << " " << p.second << endl << endl;
 
 
 // Why we use pair => for maintain relationship between two things.
 
 
-    int a[] = {1, 2, 3};
-    int b[] = {2, 3, 4};
-    // Here a and b is related to each other....
+    // int a[] = {1, 2, 3};
+    // int b[] = {2, 3, 4};
+    // // Here a and b is related to each other....
 
-    pair<int, int> p_array[3];   // Always used vector of array for this....
-    p_array[0] = {1, 2};
-    p_array[1] = {2, 3};
-    p_array[2] = {3, 4};
+    // pair<int, int> p_array[3];   // Always used vector of array for this....
+    // p_array[0] = {1, 2};
+    // p_array[1] = {2, 3};
+    // p_array[2] = {3, 4};
 
-    swap(p_array[0], p_array[2]);
+    // swap(p_array[0], p_array[2]);
 
-    // For print this pair array...
-    for (int i = 0; i < 3; i++)
-    {
-        cout << p_array[i].first << " " << p_array[i].second << endl;
-    }
+    // // For print this pair array...
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     cout << p_array[i].first << " " << p_array[i].second << endl;
+    // }
     
 
+// ===================================================================================================
+
+
+// Vectors
+
+
+    int a[10];   // 10 size array of int type...
+    vector<int> v;   // Currently size is 0. But we can vary its size.
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    
     return 0;
 } 
