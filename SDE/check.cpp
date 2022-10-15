d42a77714439904d73879fee600e591bb9a95a4c
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-10-15 11:25:34 +0530

    Update file

--- SDE/check.cpp
+++ SDE/check.cpp
@@ -9,6 +9,13 @@ int main()
     pair<int, string> p;
     // p = make_pair(2, "Akhil");
     p = {2, "Akhil Upadhyay"};
+    pair <int, string> p1 = p;    // By value
+    p1.first = 10;
+    cout << p1.first << " " << p1.second << endl;
+    cout << p.first << " " << p.second << endl;
+    pair <int, string> &p2 = p;   // By reference
+    p2.second = "Nikhil";
+    cout << p2.first << " " << p2.second << endl;
     cout << p.first << " " << p.second << endl;
     return 0;
 } 
