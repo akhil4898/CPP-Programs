713427638cd7ddac6ae71162e87a0658e6736ca0
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-10-15 11:16:24 +0530

    Update file

--- SDE/check.cpp
+++ SDE/check.cpp
@@ -7,7 +7,8 @@ using namespace std;
 int main()
 {
     pair<int, string> p;
-    p = make_pair(2, "Akhil");
+    // p = make_pair(2, "Akhil");
+    p = {2, "Akhil Upadhyay"};
     cout << p.first << " " << p.second << endl;
     return 0;
 } 
