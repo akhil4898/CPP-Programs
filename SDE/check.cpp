3c35daec88b39c5d436235f7a4c9497b0439625a
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-10-15 18:38:40 +0530

    Update file

--- SDE/check.cpp
+++ SDE/check.cpp
@@ -229,18 +229,32 @@ int main()
 // Iterations...
 
 
-    vector<int> v = {2, 3, 4, 5, 6, 7};
-    for (int i = 0; i < v.size(); i++)
+    // vector<int> v = {2, 3, 4, 5, 6, 7};
+    // for (int i = 0; i < v.size(); i++)
+    // {
+    //     cout << v[i] << " ";
+    // }
+    // cout << endl;
+    // // declare Iterator...
+    // vector<int> :: iterator it = v.begin();
+    // cout << (*(it + 1)) << endl;
+    // // print elements using iterator...
+    // for (it = v.begin(); it != v.end(); it++)
+    // {
+    //     cout << (*it) << " ";
+    // }
+    // cout << endl;
+    // // declare vector of pair to iterate elements..
+    vector<pair<int, int> > v_p = {{1,2}, {2,3}, {4,5}};    
+    vector<pair<int,int>> ::iterator it;
+    for ( it = v_p.begin(); it != v_p.end(); it++)
     {
-        cout << v[i] << " ";
+        cout << (*it).first << " " << (*it).second << endl;
     }
-    cout << endl;
+    
 
     
-    // declare Iterator...
-    vector<int> :: iterator it = v.begin();
-    cout << (*it) << endl;
-    
+
     
     
     
