ea8ab1f0f763f83cb5367672e12a8083c83ff6ae
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-10-18 22:08:13 +0530

    Update file

--- SDE/check.cpp
+++ SDE/check.cpp
@@ -200,18 +200,18 @@ int main()
 
 // Vector of pair...
 
-    // vector<pair<int, int>> v = {{1, 2}, {2, 3},{3, 4}};
-    // vector<pair<int, int>> v;
-    // int n;
-    // cin >> n;
-    // for (int i = 0; i < n; i++)
-    // {
-    //     int x, y;
-    //     cin >> x >> y;
-    //     // v.push_back(make_pair(x, y));
-    //     v.push_back({x, y});
-    // }
-    // printVec(v);
+    vector<pair<int, int>> v = {{1, 2}, {2, 3},{3, 4}};
+    vector<pair<int, int>> v;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        // v.push_back(make_pair(x, y));
+        v.push_back({x, y});
+    }
+    printVec(v);
 
 
 
